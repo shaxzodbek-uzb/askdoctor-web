f389bf852ad7ddc8088c130d51e3bd5637b7e1aa
Shaxzodbek Qambaraliyev <shaxzodbek.qambaraliyev@gmail.com>
2019-08-08 20:40:33 +0500

    routes

--- src/router/index.js
+++ src/router/index.js
@@ -8,27 +8,39 @@ import EncyclopediaRouter from './modules/encyclopedia'
 
 Vue.use(Router)
 
-export default new Router({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    ...ClinicRouter,
-    ...DoctorRouter,
-    ...ArticleRouter,
-    ...EncyclopediaRouter
-    // {
-    //   path: "/about",
-    //   name: "about",
-    //   // route level code-splitting
-    //   // this generates a separate chunk (about.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () =>
-    //     import(/* webpackChunkName: "about" */ "@/views/About.vue")
-    // }
-  ]
+export const constantRoutes = [
+  {
+    path: '/',
+    name: 'home',
+    component: Home
+  },
+  ...ClinicRouter,
+  ...DoctorRouter,
+  ...ArticleRouter,
+  ...EncyclopediaRouter
+  // {
+  //   path: "/about",
+  //   name: "about",
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () =>
+  //     import(/* webpackChunkName: "about" */ "@/views/About.vue")
+  // }
+]
+
+const createRouter = () => new Router({
+  // mode: 'history', // require service support
+  scrollBehavior: () => ({ y: 0 }),
+  routes: constantRoutes
 })
+
+const router = createRouter()
+
+// Detail see: https://github.com/vuejs/vue-router/issues/1234#issuecomment-357941465
+export function resetRouter() {
+  const newRouter = createRouter()
+  router.matcher = newRouter.matcher // reset router
+}
+
+export default router
---
+++ src/settings.js
@@ -0,0 +1,18 @@
+module.exports = {
+
+  title: 'AskDoctor',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: false,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  sidebarLogo: false,
+  tagsView: true
+
+}
